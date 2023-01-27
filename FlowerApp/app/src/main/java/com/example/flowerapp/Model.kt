df739d16cac4a7df377a7ffcdcf2352da439b1c5
atla_naveen <an5867@srmist.edu.in>
2023-01-28 00:20:32 +0530

    Uploaded File

---
+++ FlowerApp/app/src/main/java/com/example/flowerapp/Model.kt
@@ -0,0 +1,24 @@
+package com.example.flowerapp
+
+class Model {
+
+    val listOfFlowerObjects: MutableList<Flower> = mutableListOf(
+
+        Flower("abutilon", R.drawable.abutilon),
+        Flower("acacia", R.drawable.acacia),
+        Flower("aconite", R.drawable.aconite),
+        Flower("bluestar", R.drawable.bluestar),
+        Flower("borage", R.drawable.borage),
+        Flower("broom", R.drawable.broom),
+        Flower("camellia", R.drawable.camellia),
+        Flower("clover", R.drawable.clover),
+        Flower("coral", R.drawable.coral),
+        Flower("desertrose", R.drawable.desertrose),
+        Flower("abutilon", R.drawable.abutilon),
+        Flower("bluestar", R.drawable.bluestar),
+        Flower("desertrose", R.drawable.desertrose),
+        Flower("broom", R.drawable.broom),
+        Flower("clover", R.drawable.clover),
+
+    )
+}
